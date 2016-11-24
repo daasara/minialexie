3e82de6d66dc426887591cb90b2d066203eeaab2
Heitor Chang <heitorchang@gmail.com>
2016-11-24 17:13:25 -0200

    index with budget and percentage spent

--- benny/views.py
+++ benny/views.py
@@ -1,3 +1,5 @@
+from math import ceil
+
 from django.shortcuts import render
 
 from .models import AccountType, Account, Transaction
@@ -14,13 +16,29 @@ def index(request):
     from_date = parse_from_date(request)
     to_date = parse_to_date(request)
 
-    accountTypes = AccountType.objects.filter(user=request.user).order_by('order')
-
-    accountTypeBalances = {}
+    accountTypes = AccountType.objects.filter(user=request.user).order_by('order', 'name')
 
     for accountType in accountTypes:
-        accountTypeBalances[accountType.id] = Account.objects.filter(user=request.user, account_type=accountType
+        # call balance here, using the parsed from and to dates
+        accountType.accountSummaries = []
+
+        # sort by order field, using a temporary variable
+        accountTypeAccounts = Account.objects.filter(user=request.user, account_type=accountType.id).order_by('order', 'name')
+        
+        for account in accountTypeAccounts:
+            # fill in balances in order
+            budget = account.budget
+            datesBalance = account.balance(from_date, to_date)
+            if budget > 0:
+                percentSpent = str(ceil(datesBalance / budget * 100)) + "%"
+            else:
+                percentSpent = 0
+                
+            accountType.accountSummaries.append(
+                { 'name': account.name,
+                  'budget': budget, 
+                  'datesBalance': datesBalance,
+                  'percentSpent': percentSpent, })
 
     return render(request, 'benny/index.html',
                   { 'accountTypes': accountTypes })
-                  
