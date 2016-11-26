ca6e3c42c416fb7076b33ae54fb5aa01dd586038
Heitor Chang <heitorchang@gmail.com>
2016-11-26 19:45:41 -0200

    time period

--- minialexie/settings.py
+++ minialexie/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'sitewide.apps.SitewideConfig',
     'alexie.apps.AlexieConfig',
     'benny.apps.BennyConfig',
+    'django.contrib.humanize',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
